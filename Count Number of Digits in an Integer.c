b1377c32b7856d202bbf99ce28a269833889aac2
ChandanKumar007 <57029009+ChandanKumar007@users.noreply.github.com>
2019-10-26 13:18:07 +0530

    Update Count Number of Digits in an Integer.c

--- Count Number of Digits in an Integer.c
+++ Count Number of Digits in an Integer.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 int main()
 {
-    long long n;
+    long long int n;
     int count = 0;
     printf("Enter an integer: ");
     scanf("%lld", &n);
     while(n != 0)
     {
-        // n = n/10
-        n /= 10;
+        n = n/10;
         ++count;
     }
     printf("Number of digits: %d", count);
